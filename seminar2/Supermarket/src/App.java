df8bc4672660ded3384b49551bde6a524b80ae53
Adilzhan Samigulov <a.samighulov2@gmail.com>
2023-05-31 07:26:52 +0600

    starting seminar 2

--- seminar2/Supermarket/src/App.java
+++
@@ -1,7 +0,0 @@
-import Domen.Bottle;
-import Domen.HotDrink;
-import Domen.Product;
-import VendingMachine.VendingMachine;
-public class App {
-    public static void main(String[] args) throws Exception {
-}
